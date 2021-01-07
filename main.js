d5cca23fa62010ee7212255e3876ef5c2f19035e
FabulousStarfish <71825170+FabulousStarfish@users.noreply.github.com>
2021-01-07 16:41:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,30 @@ Webcam.set({
     }
 });
 Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="image_result" src="'+data_uri+'">';
+    });
+}
+console.log("Ml5 Version",ml5.version);
+
+classifier=ml5.imageClassifier("MobileNet",modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Has Been Loaded");
+}
+function check(){
+    img=document.getElementById("image_result");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result)
+        answer=result[0].label;
+        document.getElementById("output").innerHTML=answer;
+    }
+}
